e30277f843d0b0c4323e430927cfeaabe8e6eeff
Eddy Guo <eddyyanruguo@gmail.com>
2021-05-17 17:00:23 -0400

    Remove default Next.js page

---
+++ prisma/migrations/20210517203350_/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "Title" TEXT NOT NULL,
+    "Year" INTEGER NOT NULL,
+    "Description" TEXT NOT NULL,
+    "Slug" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
